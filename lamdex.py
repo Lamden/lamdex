96927a60dd409e4bf6ba78d8e5574f41fbf95665
StuartFarmer <email@stuartfarmer.me>
2018-02-15 19:57:14 -0800

    it works (kinda)

--- lamdex.py
+++ lamdex.py
@@ -24,9 +24,9 @@ A TX is split into an input and an output fulfilled in two parts.
 wallets = [secrets.token_hex(4) for x in range(10)]
 
 
-def random_stake():
+def random_stake(range=(10, 50)) -> object:
     return {
-        'amount': random.randint(10, 50),
+        'amount': random.randint(range[0], range[1]),
         'owner': random.sample(wallets, 1)[0],
         'id': secrets.token_hex(8)
     }
@@ -34,24 +34,26 @@ def random_stake():
 
 major_liquid = [random_stake() for x in range(100)]
 
-tx = {'amount': None, 'owner': None, 'id': None}
-
 minor_liquid = {
     'EOS': [],
     'ICX': [],
     'OMG': []
 }
 
+
 def tx_in(token, tx):
 
     # determine the forex exchange rate between the base currency
     # and major liquid market
-    fx = token/return_rate()
+
+    fx = return_rate()/return_rate()
 
     # setup how much to fill and the out transaction for when it is filled
     to_fill = fx * tx['amount']
     filled = 0
     out_tx = {'amount': 0, 'owner': tx['owner'], 'id': secrets.token_hex(8)}
+
+    print('attempting to liquidate {}\n'.format(tx))
 
     # try to completely fill the order with the current major liquid
     while filled < to_fill:
@@ -72,20 +74,34 @@ def tx_in(token, tx):
             to_give /= fx
 
             # shoot off a transaction to illiquid which attempts to fill it there (potential recursion here)
+            print('complete liquidation of {}'.format(ml))
+            print('{}/{} left to liquidate'.format(filled, to_fill))
+            print('sending to minor markets\n')
 
             # destroy the order
             major_liquid.pop(0)
 
         else:
             # otherwise, the entire trade could not be liquidated and we have to alter is slightly
+            print('partial liquidation of {}'.format(ml))
             difference = to_fill - filled
             ml['amount'] -= difference
+
+            print('only {} liquidated'.format(difference))
 
             to_give = to_fill / difference
             to_give /= fx
 
             # shoot off a transaction to illiquid which attempts to fill it there (potential recursion here)
 
+            print('sending to minor markets\n')
+
+            # lets redundantly break just in case
+            break
+
 
 def return_rate():
     return 1
+
+
+tx_in('EOS', random_stake((100, 200)))
